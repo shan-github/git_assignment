6fbf3d1e5b9374f94a41e6b81338118de0c643f9
Shantanu Bahuguna <bshantanu1@gmail.com>
2018-11-13 13:45:17 +0530

    added comments

--- poly.cpp
+++ poly.cpp
@@ -1,7 +1,9 @@
+// C++ program for addition of two polynomials 
+// using Linked Lists 
 #include<bits/stdc++.h> 
 using namespace std; 
   
-
+// Node structure containing power and coefficient of variable 
 struct Node 
 { 
     int coeff; 
@@ -9,7 +11,7 @@ struct Node
     struct Node *next; 
 }; 
               
-
+// Function to create new node 
 void create_node(int x, int y, struct Node **temp) 
 { 
     struct Node *r, *z; 
@@ -34,13 +36,13 @@ void create_node(int x, int y, struct Node **temp)
     } 
 } 
   
-
+// Function Adding two polynomial numbers 
 void polyadd(struct Node *poly1, struct Node *poly2, struct Node *poly) 
 { 
 while(poly1->next && poly2->next) 
     { 
-
-
+        // If power of 1st polynomial is greater then 2nd, then store 1st as it is 
+        // and move its pointer 
         if(poly1->pow > poly2->pow) 
         { 
             poly->pow = poly1->pow; 
@@ -48,8 +50,8 @@ while(poly1->next && poly2->next)
             poly1 = poly1->next; 
         } 
           
-
-
+        // If power of 2nd polynomial is greater then 1st, then store 2nd as it is 
+        // and move its pointer 
         else if(poly1->pow < poly2->pow) 
         { 
             poly->pow = poly2->pow; 
@@ -57,7 +59,7 @@ while(poly1->next && poly2->next)
             poly2 = poly2->next; 
         } 
           
-
+        // If power of both polynomial numbers is same then add their coefficients 
         else
         { 
             poly->pow = poly1->pow; 
@@ -66,7 +68,7 @@ while(poly1->next && poly2->next)
             poly2 = poly2->next; 
         } 
           
-
+        // Dynamically create new node 
         poly->next = (struct Node *)malloc(sizeof(struct Node)); 
         poly = poly->next; 
         poly->next = NULL; 
@@ -91,7 +93,7 @@ while(poly1->next || poly2->next)
     } 
 } 
   
-
+// Display Linked list 
 void show(struct Node *node) 
 { 
 while(node->next != NULL) 
@@ -103,17 +105,17 @@ while(node->next != NULL)
     } 
 } 
   
-
+// Driver  program 
 int main() 
 { 
     struct Node *poly1 = NULL, *poly2 = NULL, *poly = NULL; 
       
-
+    // Create first list of 5x^2 + 4x^1 + 2x^0 
     create_node(5,2,&poly1); 
     create_node(4,1,&poly1); 
     create_node(2,0,&poly1); 
       
-
+    // Create second list of 5x^1 + 5x^0 
     create_node(5,1,&poly2); 
     create_node(5,0,&poly2); 
       
@@ -125,9 +127,10 @@ int main()
       
     poly = (struct Node *)malloc(sizeof(struct Node)); 
       
-
+    // Function add two polynomial numbers 
     polyadd(poly1, poly2, poly); 
       
+    // Display resultant List 
     printf("\nAdded polynomial: "); 
     show(poly); 
   
